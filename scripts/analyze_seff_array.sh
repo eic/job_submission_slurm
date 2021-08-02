a8a25849710975626532c59ae8c83d44f51efc29
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-08-02 00:43:09 +0000

    Slurm array analysis

---
+++ scripts/analyze_seff_array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Usage:
+# cat << EOF | xargs -o ./analyze_seff_array.sh | grep 'Job Wall-clock time' | cut -d: -f2-
+# cat << EOF | xargs -o ./analyze_seff_array.sh | grep 'Average Memory Utilized' | cut -d: -f2-
+# cat << EOF | xargs -o ./analyze_seff_array.sh | grep "CPU Efficiency" | cut -d: -f2 | awk '{print$1}'
+
+
+while [ $# -ge 1 ] ; do
+  seff-array ${1}
+  shift
+done
