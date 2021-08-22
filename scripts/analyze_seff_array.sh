77a83f806b75e5a21df938bc644cc5dd53fd8ea5
Wouter Deconinck <wdconinc@gmail.com>
2021-08-22 16:38:38 -0400

    Added scripts to analyze_seff_array command usage examples

--- scripts/analyze_seff_array.sh
+++ scripts/analyze_seff_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Usage:
-# cat << EOF | xargs -o ./analyze_seff_array.sh | grep 'Job Wall-clock time' | cut -d: -f2-
-# cat << EOF | xargs -o ./analyze_seff_array.sh | grep 'Average Memory Utilized' | cut -d: -f2-
-# cat << EOF | xargs -o ./analyze_seff_array.sh | grep "CPU Efficiency" | cut -d: -f2 | awk '{print$1}'
+# cat << EOF | xargs -o scripts/analyze_seff_array.sh | grep 'Job Wall-clock time' | cut -d: -f2-
+# cat << EOF | xargs -o scripts/analyze_seff_array.sh | grep 'Average Memory Utilized' | cut -d: -f2-
+# cat << EOF | xargs -o scripts/analyze_seff_array.sh | grep "CPU Efficiency" | cut -d: -f2 | awk '{print$1}'
 
 
 while [ $# -ge 1 ] ; do
