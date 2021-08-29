16b4d3eefb12c6f40ae408632fc60306b76a294e
Wouter Deconinck <wdconinc@jlab.org>
2021-08-29 19:49:46 -0400

    SR submission file

---
+++ scripts/SR.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ACCOUNT=${1:-eic}
+EMAIL=${2:-${USER}@jlab.org}
+
+export JUGGLER_TAG=nightly
+export CAMPAIGNS=$(realpath ~/EIC/campaigns)
+export RECONSTRUCTION=$(realpath ~/EIC/benchmarks/reconstruction_benchmarks)
+export USEHEPMC3=true
+
+SBATCH="sbatch --account=${ACCOUNT} --mail-user=${EMAIL} --parsable --array=1-50 --time=24:00:00"
+
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25098.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25099.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25100.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25101.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25111.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25112.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25113.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25114.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25115.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25116.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25117.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25118.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25119.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25120.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25130.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25131.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25132.hepmc 10000
+${SBATCH} scripts/submit.sh hepmc3 EVGEN/SR/SR.10GeV_5kVthreshold_hepmc/25133.hepmc 10000
+
