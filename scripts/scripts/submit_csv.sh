44d9d1c8c6fe7995657c59414b11297ca23578bf
Wouter Deconinck <wdconinc@gmail.com>
2021-11-11 16:19:42 +0000

    submit_csv.sh for slurm

---
+++ scripts/scripts/submit_csv.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -Euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# check arguments
+if [ $# -lt 2 ] ; then
+  echo "Usage: "
+  echo "  $0 <template> <type> <file or url> [target hours = 2]"
+  exit
+fi
+
+# Project configuration
+BASEURL="https://eicweb.phy.anl.gov/api/v4/projects/491/jobs/artifacts/main/raw/results/datasets/timings/"
+BASEJOB="?job=collect"
+
+# Parse arguments
+# - condor template
+TEMPLATE=${1}
+shift
+# - type of simulation
+TYPE=${1}
+shift
+# - input file
+FILE=${1}
+shift
+# - target hours
+TARGET=${1:-2}
+shift
+
+# Parse input URI
+if [ -f "${FILE}" ] ; then
+  echo "Using local file as input"
+  INPUT="cat ${FILE}"
+else
+  echo "Using ${BASEURL}${FILE}${BASEJOB} as input"
+  INPUT="curl ${BASEURL}${FILE}${BASEJOB}"
+fi
+
+# Loop over input
+SBATCH="sbatch --mail-user=${EMAIL:-${USER}@jlab.org} --parsable --requeue --time=$((2*TARGET)):00:00"
+${INPUT} | grep -v ^curl | while IFS="," read file ntotal dt0 dt1 ; do
+  nevents=$(echo "n=(3600*$TARGET-$dt0)/$dt1; if (n>$ntotal) print($ntotal) else print(n)" | bc)
+  nchunks=$(echo "n=$ntotal/$nevents; if (n==0) print(1) else print(n)" | bc)
+  ${SBATCH} --array=1-${nchunks} $(dirname $0)/submit.sh ${TYPE} ${file} ${nevents}
+done
