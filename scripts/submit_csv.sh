85196e52afe90fd7bacce99d1db3960125e08f2c
Wouter Deconinck <wdconinc@jlab.org>
2021-11-12 19:18:27 -0500

    Make submit_csv.sh work on slurm

--- scripts/submit_csv.sh
+++ scripts/submit_csv.sh
@@ -15,9 +15,6 @@ BASEURL="https://eicweb.phy.anl.gov/api/v4/projects/491/jobs/artifacts/main/raw/
 BASEJOB="?job=collect"
 
 # Parse arguments
-# - condor template
-TEMPLATE=${1}
-shift
 # - type of simulation
 TYPE=${1}
 shift
@@ -31,16 +28,20 @@ shift
 # Parse input URI
 if [ -f "${FILE}" ] ; then
   echo "Using local file as input"
-  INPUT="cat ${FILE}"
+  INPUT=(cat ${FILE})
 else
   echo "Using ${BASEURL}${FILE}${BASEJOB} as input"
-  INPUT="curl ${BASEURL}${FILE}${BASEJOB}"
+  INPUT=(curl ${BASEURL}${FILE}${BASEJOB})
 fi
 
 # Loop over input
-SBATCH="sbatch --mail-user=${EMAIL:-${USER}@jlab.org} --parsable --requeue --time=$((2*TARGET)):00:00"
-${INPUT} | grep -v ^curl | while IFS="," read file ntotal dt0 dt1 ; do
+SBATCH=(sbatch --mail-user=${EMAIL:-${USER}@jlab.org} --parsable --requeue --time=$((2*TARGET)):00:00)
+"${INPUT[@]}" | grep -v ^curl | while IFS="," read file ntotal dt0 dt1 ; do
+  if [ -z "${file}" ] ; then
+    continue
+  fi
   nevents=$(echo "n=(3600*$TARGET-$dt0)/$dt1; if (n>$ntotal) print($ntotal) else print(n)" | bc)
   nchunks=$(echo "n=$ntotal/$nevents; if (n==0) print(1) else print(n)" | bc)
-  ${SBATCH} --array=1-${nchunks} $(dirname $0)/submit.sh ${TYPE} ${file} ${nevents}
-done
+  echo "${file}: ${dt0}+N*${dt1} s, for ${nchunks} chunks for ${TARGET} hours. Submit? [Y,n] "
+  "${SBATCH[@]}" --array=1-${nchunks} $(dirname $0)/submit.sh ${TYPE} ${file} ${nevents}
+done | tee $(basename ${FILE} .csv)-$(date --iso-8601=minutes).log
